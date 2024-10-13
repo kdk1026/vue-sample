68ec0d8d9e6eb9a7ca2bb9a01f81d51c103246c8
kdk1026 <kdk1026@gmail.com>
2024-10-13 21:05:37 +0900

    타이틀 동적 변경

--- src/router/index.js
+++ src/router/index.js
@@ -9,33 +9,43 @@ import ToastPage from "../pages/ToastPage.vue";
 import InputDate from "../pages/InputDate.vue";
 import VueDatePicker from "../pages/VueDatePicker.vue";
 
+const title = process.env.VUE_APP_TITLE;
+
 const routes = [
     {
         path: '/', component: Main,
         children: [
             {
-                path: 'captcha', component: Captcha
+                path: 'captcha', component: Captcha,
+                meta: { title: `${title} | Captcha` }
             },
             {
-                path: 'pagination', component: Pagination
+                path: 'pagination', component: Pagination,
+                meta: { title: `${title} | Pagination` }
             },
             {
-                path: 'calendar', component: Calendar
+                path: 'calendar', component: Calendar,
+                meta: { title: `${title} | Calendar` }
             },
             {
-                path: 'map', component: Map
+                path: 'map', component: Map,
+                meta: { title: `${title} | Naver Map` }
             },
             {
-                path: 'swiper', component: SwiperPage
+                path: 'swiper', component: SwiperPage,
+                meta: { title: `${title} | Swiper` }
             },
             {
-                path: 'toast', component: ToastPage
+                path: 'toast', component: ToastPage,
+                meta: { title: `${title} | Toast` }
             },
             {
-                path: 'inpput-date', component: InputDate
+                path: 'inpput-date', component: InputDate,
+                meta: { title: `${title} | InputDate` }
             },
             {
-                path: 'datepicker', component: VueDatePicker
+                path: 'datepicker', component: VueDatePicker,
+                meta: { title: `${title} | DatePicker` }
             }
         ]
     }
@@ -44,6 +54,10 @@ const routes = [
 const router = createRouter({
     history: createWebHistory(),
     routes
+});
+
+router.afterEach((to) => {
+    document.title = to.meta.title || title;
 });
 
 export default router;
