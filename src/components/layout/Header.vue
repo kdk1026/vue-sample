9359f27b42c3f33b17d02a49b9631c468a0d7938
kdk1026 <kdk1026@gmail.com>
2025-02-12 20:22:54 +0900

    수정

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,15 +1,17 @@
 <template>
-    <nav>
-        <router-link to="/">Home</router-link>
-        <router-link to="/captcha">Captcha</router-link> 
-        <router-link to="/pagination">Pagination</router-link>
-        <router-link to="/calendar">Calendar</router-link>
-        <router-link to="/map">Naver Map</router-link>
-        <router-link to="/swiper">Swiper</router-link>
-        <router-link to="/toast">Toast</router-link>
-        <router-link to="/inpput-date">Input Date</router-link>
-        <router-link to="/datepicker">DatePciker</router-link>
-        <router-link to="/social-login">소셜 로그인</router-link>
+    <nav className="menu">
+        <ul>
+            <li><router-link to="/">Home</router-link></li>
+            <li><router-link to="/captcha">Captcha</router-link></li>
+            <li><router-link to="/pagination">Pagination</router-link></li>
+            <li><router-link to="/calendar">Calendar</router-link></li>
+            <li><router-link to="/map">Naver Map</router-link></li>
+            <li><router-link to="/swiper">Swiper</router-link></li>
+            <li><router-link to="/toast">Toast</router-link></li>
+            <li><router-link to="/inpput-date">Input Date</router-link></li>
+            <li><router-link to="/datepicker">DatePciker</router-link></li>
+            <li><router-link to="/social-login">소셜 로그인</router-link></li>
+        </ul>
     </nav>
 </template>
 
--- src/pages/EmptyPage.vue
+++ src/pages/EmptyPage.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
-        <h2>잘못된 접근입니다.</h2>
-        <router-link to="/">돌아가기</router-link>
+        <h2 class="msg">잘못된 접근입니다.</h2>
+        <router-link to="/" class="btn">돌아가기</router-link>
     </div>
 </template>
 
@@ -9,6 +9,24 @@
 
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
+    .msg {
+        color: #ff0000;
+        text-align: center;
+        font-size: 24px;
+        margin-top: 20px;
+    }
 
+    .btn {
+        display: block;
+        width: 100%;
+        padding: 10px 0;
+        margin-top: 20px;
+        text-align: center;
+        background-color: #007bff;
+        color: #fff;
+        text-decoration: none;
+        border-radius: 5px;
+        box-sizing: border-box;
+    }
 </style>
